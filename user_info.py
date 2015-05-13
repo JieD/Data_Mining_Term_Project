cd0b7caace44c0417655c2b53481a94466e8087c
jied <jied314@mail.sfsu.edu>
2015-05-12 23:19:02 -0700

    start collect user info

--- user_info.py
+++ user_info.py
@@ -1,1 +1,18 @@
-__author__ = 'jie'
+from pprint import pprint
+import json
+import lib
+import reddit_client
+
+client = reddit_client.login(lib.USERNAME, lib.PASSWORD, lib.USER_AGENT)
+#parameters = {'sort': 'new', 'limit': 100, 'restrict_sr': 'on', 'syntax': 'cloudsearch'}
+url = r'http://www.reddit.com/user/{un}/about/.json'.format(un='ultimate_loser')
+#url = r'http://www.reddit.com/search.json?q=subreddit:Random_Acts_Of_Pizza+author:ultimate_loser'
+r = client.get(url)
+print 'sent URL is', r.url
+j = json.loads(r.text)
+pprint(j)
+#comments = j['data']['children']
+#num_comment = len(comments)
+#print "user has {0} comments".format(num_comment)
+#print data['comment_karma'], "\r\n", data['link_karma'], data['created'], data['created_utc']
+
