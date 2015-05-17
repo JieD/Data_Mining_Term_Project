2d801c0693a7a4bf0116101a6b5bf2b30e01a42a
jied <jied314@mail.sfsu.edu>
2015-05-17 02:43:37 -0700

    check whether username exists

--- user_info.py
+++ user_info.py
@@ -5,8 +5,8 @@ import reddit_client
 
 client = reddit_client.login(lib.USERNAME, lib.PASSWORD, lib.USER_AGENT)
 #parameters = {'sort': 'new', 'limit': 100, 'restrict_sr': 'on', 'syntax': 'cloudsearch'}
-url = r'http://www.reddit.com/user/{un}/about/.json'.format(un='ultimate_loser')
-#url = r'http://www.reddit.com/search.json?q=subreddit:Random_Acts_Of_Pizza+author:ultimate_loser'
+#url = r'http://www.reddit.com/user/{un}/about/.json'.format(un='thanks')
+url = r'http://www.reddit.com/search.json?q=subreddit:Random_Acts_Of_Pizza+author:the'
 r = client.get(url)
 print 'sent URL is', r.url
 j = json.loads(r.text)
@@ -16,3 +16,9 @@ pprint(j)
 #print "user has {0} comments".format(num_comment)
 #print data['comment_karma'], "\r\n", data['link_karma'], data['created'], data['created_utc']
 
+def is_user_name(name):
+    client = reddit_client.login(lib.USERNAME, lib.PASSWORD, lib.USER_AGENT)
+    url = r'http://www.reddit.com/search.json?q=subreddit:Random_Acts_Of_Pizza+author:{0}'.format(name)
+    r = client.get(url)
+    print 'sent URL is', r.url
+    j = json.loads(r.text)
