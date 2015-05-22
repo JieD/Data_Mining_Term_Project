aa98baba2406b8625d0dd1d3567354da95ad1980
jied <jied314@mail.sfsu.edu>
2015-05-22 12:54:11 -0700

    main file for easy execution

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-__author__ = 'jie'
+import preprocess_label_edited
+import preprocess_text
+import text_analysis
+import lib
+
+preprocess_label_edited.main()
+preprocess_text.main()
+text_analysis.main()
